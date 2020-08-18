a9854a72bf978ec6b9970e20ab5377ac875ade50
AsQuiet <65712624+AsQuiet@users.noreply.github.com>
2020-08-18 09:38:14 +0200

    removed syntax code

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 
-using DataKeep.Tokens;
-using DataKeep.ParserTypes;
-using DataKeep.Syntax;
-
 namespace DataKeep
 {
     class Program
@@ -12,34 +8,23 @@ namespace DataKeep
         {
 
             string macPath = "/Users/QuinnyBoy/Documents";
-            string winPath = "E:\\PROJECTS";               
+            //string winPath = "E:\\PROJECTS";               
 
             DebugDK.StartStopwatch("main");
             DebugDK.SetLog(true);
 
             DebugDK.StartStopwatch("lexer");
-            Lexer lexer = new Lexer(new FileHandler(winPath + "/DataManager/DataKeep/data.dk"));
+            Lexer lexer = new Lexer(new FileHandler(macPath + "/DataManager/DataKeep/data.dk"));
             lexer.LexAllLines();
             DebugDK.StopStopwatch("lexer");
 
             DebugDK.StartStopwatch("parser");
+
             Parser parser = new Parser(lexer);
             parser.ParseAllLines();
             parser.GiveStructInheritance();
             DebugDK.StopStopwatch("parser");
             parser.PrintAllData();
-
-            /*DebugDK.StartStopwatch("syntaxparser");
-            SyntaxParser syntaxParser = new SyntaxParser(new FileHandler(winPath + "/DataManager/DataKeep/main.dks"));
-            syntaxParser.ParseAllLines();
-            DebugDK.StopStopwatch("syntaxparser");
-            syntaxParser.PrintAllData();
-
-            DebugDK.StartStopwatch("linker");
-            Linker linker = new Linker(syntaxParser, parser);
-            linker.Convert();
-            linker.OutputToFile("output.csbutnotcs");
-            DebugDK.StopStopwatch("linker");*/
 
             DebugDK.StopStopwatch("main");
 
